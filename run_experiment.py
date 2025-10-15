5078f440435fbbf0b53e7ff5cc28d09997c5ecf6
AndreaTaverna2001 <andrealuca.taverna@uzh.ch>
2025-10-15 16:05:25 +0200

    commit

--- run_experiment.py
+++ run_experiment.py
@@ -1,13 +1,18 @@
+import sys
+import os
+
+# Add the current project directory to Python's path
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
 import hydra
 from omegaconf import DictConfig
 from benchmarl.run import hydra_experiment
+import my_envs.inventory_duopoly_pz
 
-# This is the entry point for the experiment.
-# The @hydra.main decorator takes care of loading the configuration.
 @hydra.main(
     version_base=None,
-    config_path="conf",  # Look for configs in the 'conf' directory
-    config_name="config",    # Load 'config.yaml' by default
+    config_path="conf",
+    config_name="config",
 )
 def run(cfg: DictConfig) -> None:
     """
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="algocollusion",
+    version="0.1",
+    packages=find_packages(),
+)
